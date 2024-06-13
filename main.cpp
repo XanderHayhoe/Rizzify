9136c1e2810eaa5ef91ccbdd0a6f6f0f11a65a22
XanderHayhoe <xanderhayhoe@gmail.com>
2024-06-12 20:20:59 -0400

    lib complete

---
+++ main.cpp
@@ -0,0 +1,13 @@
+
+
+/* examples of using the powerful sigma.h lib*/
+
+#include "sigma.h"
+#include <stdio.h>
+#include <string.h>
+Sigma main(Sigma argc, yap* argv){
+    yap rizz = (yap)gib(sizeof(yap) * 100);
+    Yappify(rizz, "sigma");
+    glaze("%s\n", rizz); 
+    return wagwan;
+}
---
+++ sigma.h
@@ -0,0 +1,31 @@
+/*
+    header file to create standard typedefs to rot your C++
+
+
+standard types defined:
+int 
+double 
+short 
+std::string 
+void 
+unsigned int 
+
+
+*/
+
+#include <string>
+
+#define wagwan 0
+#define gib malloc
+#define glaze printf
+#define Yappify strcpy
+
+typedef int Sigma;
+typedef std::string Yap;
+typedef double Skibidi;
+typedef short BabyGronk;
+typedef void Ohio;
+typedef unsigned int Beta;
+typedef long Omega;
+typedef float Toilet; 
+typedef char* yap;
